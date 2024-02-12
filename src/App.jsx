bc1155effe50099a520527e8dff70b59308e75d2
Mufaro1410 <fungurayimufaro@gmail.com>
2024-02-12 17:11:30 +0200

    Accordion Project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Accordion from './components/accordion'
+
+
+function App() {
+
+  return (
+    <div>
+      {/* Accordion */}
+      <Accordion />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/accordion/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id:'1',
+        question: "What is Accordion",
+        answer: "bla bla bla blas"
+    },
+    {
+        id:'2',
+        question: "What is Accordion",
+        answer: "bla bla bla blas"
+    },
+    {
+        id:'3',
+        question: "What is Accordion",
+        answer: "bla bla bla blas"
+    },
+    {
+        id:'4',
+        question: "What is Accordion",
+        answer: "bla bla bla blas"
+    },
+]
+
+export default data
