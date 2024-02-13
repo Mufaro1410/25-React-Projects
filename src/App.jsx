662eb7e12ae16afcd3794d3f9d0f4be03fde7803
Mufaro1410 <fungurayimufaro@gmail.com>
2024-02-13 17:47:13 +0200

    Star Rating Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import './App.css'
 import Accordion from './components/accordion'
 import RandomColour from './components/random-colour'
+import StarRating from './components/star-rating'
 
 
 function App() {
 
   return (
     <div>
-      {/* Accordion */}
-      {/* <Accordion /> */}
+      {/* Accordion Component */}
+      <Accordion />
 
-      {/* Random Colour */}
-      <RandomColour/>
+      {/* Random Colour Component */}
+      <RandomColour />
+
+      {/* Star Rating Component*/}
+      <StarRating noOfStars={10}/>
     </div>
   )
 }
