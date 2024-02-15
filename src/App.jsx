99df454b6a006a471a58c84a9b6cc36eae819d6e
Mufaro1410 <fungurayimufaro@gmail.com>
2024-02-15 22:19:52 +0200

    Image Slider Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Accordion from './components/accordion'
+import ImageSlider from './components/image-slider'
 import RandomColour from './components/random-colour'
 import StarRating from './components/star-rating'
 
@@ -9,13 +10,16 @@ function App() {
   return (
     <div>
       {/* Accordion Component */}
-      <Accordion />
+      {/* <Accordion /> */}
 
       {/* Random Colour Component */}
-      <RandomColour />
+      {/* <RandomColour /> */}
 
       {/* Star Rating Component*/}
-      <StarRating noOfStars={10}/>
+      {/* <StarRating noOfStars={10}/> */}
+
+      {/* Image Slider Component */}
+      <ImageSlider url={'https://picsum.photos/v2/list'} page={'1'} limit={'10'}/>
     </div>
   )
 }
