a2e50825cc02462bb918d864e0223c84e9cb1576
Mufaro1410 <fungurayimufaro@gmail.com>
2024-02-16 13:21:19 +0200

    Load More Data Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Accordion from './components/accordion'
 import ImageSlider from './components/image-slider'
+import LoadMoreData from './components/load-more-data'
 import RandomColour from './components/random-colour'
 import StarRating from './components/star-rating'
 
@@ -19,7 +20,10 @@ function App() {
       {/* <StarRating noOfStars={10}/> */}
 
       {/* Image Slider Component */}
-      <ImageSlider url={'https://picsum.photos/v2/list'} page={'1'} limit={'10'}/>
+      {/* <ImageSlider url={'https://picsum.photos/v2/list'} page={'1'} limit={'10'}/> */}
+
+      {/* Load More Data Component */}
+      <LoadMoreData />
     </div>
   )
 }
