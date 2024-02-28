5775c677da2232ec39ba3cd19b8dd4ab0b977718
Mufaro1410 <fungurayimufaro@gmail.com>
2024-02-28 21:21:08 +0200

    Tree-View Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import './App.css'
-import Accordion from './components/accordion'
-import ImageSlider from './components/image-slider'
-import LoadMoreData from './components/load-more-data'
-import RandomColour from './components/random-colour'
-import StarRating from './components/star-rating'
-
+import "./App.css";
+import Accordion from "./components/accordion";
+import ImageSlider from "./components/image-slider";
+import LoadMoreData from "./components/load-more-data";
+import RandomColour from "./components/random-colour";
+import StarRating from "./components/star-rating";
+import TreeView from "./components/tree-view";
+import menus from "./components/tree-view/data";
 
 function App() {
-
   return (
     <div>
       {/* Accordion Component */}
@@ -23,9 +23,12 @@ function App() {
       {/* <ImageSlider url={'https://picsum.photos/v2/list'} page={'1'} limit={'10'}/> */}
 
       {/* Load More Data Component */}
-      <LoadMoreData />
+      {/* <LoadMoreData /> */}
+
+      {/* Tree View Component */}
+      <TreeView menus={menus}/>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/tree-view/menu-list.jsx
@@ -0,0 +1,15 @@
+import MenuItem from "./menu-item";
+
+export default function MenuList({list = []}) {
+    return (
+        <ul className="menu-list-container">
+            {
+                list && list.length > 0 ? (
+                    list.map(listItem => (
+                        <MenuItem item={listItem}/>
+                    ))
+                ) : null
+            }
+        </ul>
+    )
+}
